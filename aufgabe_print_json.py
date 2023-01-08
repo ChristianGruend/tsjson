b3f4699c74018f8d37e2b75692a850d06e20ea0b
ChristianGruend <christian.gruender@docc.techstarter.de>
2023-01-08 17:34:31 +0100

    funktioniert nicht

---
+++ aufgabe_print_json.py
@@ -0,0 +1,8 @@
+import json
+# erstes problem, wenn ich nicht den genauen pfad angebe, findet er die datei nicht 
+data = json.loads('C:\Users\gustl\Desktop\Python-Programme\tsjson\accounts.json')
+# zweites problem, ich bekomme die file nicht ausgelesen
+print (json.dumps(data, indent =2))
+
+## ich brauch noch mehr zeit um mich mit python naeher zu befassen. 
+## leider kann ich erst am montag zur ersten nachhilfestunde.

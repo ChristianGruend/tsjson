d638fb114697da8e0e98827c67dd9365dc086888
ChristianGruend <christian.gruender@docc.techstarter.de>
2023-01-12 15:20:45 +0100

    weiter

--- aufgabe_2_json.py
+++ aufgabe_2_json.py
@@ -1,5 +1,25 @@
 import json
-
+#
 with open('tsjson/accounts.json', 'r') as f:
     data = json.load(f)
-    print (json.dumps(data, indent =2))
+#
+frage = input("Sie koennen 'alter' oder 'konto' suchen:")
+
+if frage == "alter":
+        for account in data:
+            if account["age"] > 25:
+                print(account["name"],"ist",account["age"],"Jahre alt")
+   
+#
+elif frage == "konto":
+
+    balance = []
+
+    for i in data:
+        balance.append(i["balance"])
+
+    highest = "0"
+    for number in balance: 
+        if number > highest:
+            highest = number
+    print("Der hoechste Kontostand betraegt:", highest)
